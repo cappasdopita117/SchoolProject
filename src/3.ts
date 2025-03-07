e8a21917c20cff43e9eeb303f3fcc6ae4c8729b9
cappasdopita117 <cappasdopita117@gmail.com>
2025-03-07 19:48:25 +0000

    Add 3.ts

---
+++ src/3.ts
@@ -0,0 +1,5 @@
+// Function to get a random number between 1 and 100
+const getRandomNumber = (): number => Math.floor(Math.random() * 100) + 1;
+
+// Example of using the function
+console.log(`A random number between 1 and 100 is: ${getRandomNumber()}`);

cea1bc2c3a4deda480b699aa2fc7ae43d8cf54dd
cappasdopita117 <cappasdopita117@gmail.com>
2025-04-11 10:45:07 +0000

    Add 29.ts

---
+++ src/29.ts
@@ -0,0 +1,19 @@
+// This is a simple TypeScript code to demonstrate basic operations.
+// For more complex code examples and examples of how to use classes and interfaces,
+// you can refer to the official TypeScript documentation at https://www.typescriptlang.org/docs/handbook/advanced-programming.html
+
+class SimpleClass {
+  public method1() {
+    console.log("Method 1");
+  }
+
+  public static method2(): void {
+    console.log("Static Method 2");
+  }
+}
+
+// Example usage:
+SimpleClass.method1();
+SimpleClass.static.method2();
+
+// TypeScript code for demonstration purposes
